5e34f4b414f464232e68d42052111a4c097120f5
Krutarth <krutarth1307@gmail.com>
2026-06-04 19:12:13 +0530

    Finally made food component eith the first try.

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,17 @@
+function Food(){
+
+    const food1 = "Apple";
+    const food2 = "Orange";
+
+    return(
+          <ul>
+            <li>Dhosa</li>
+            <li>Pakodi</li>
+            <li>Paua</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    )
+}
+
+export default Food
