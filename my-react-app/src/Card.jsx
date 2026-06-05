ca4cd92dbe1c1551a9dfb61764fa427abf6b8a6b
Krutarth <krutarth1307@gmail.com>
2026-06-05 10:17:29 +0530

    Finaly learnt to make a cards...

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -6,7 +6,7 @@ function Card(){
         <div className='card'>
             <img className="image" src={Icon}alt="Sova" />
             <h3 className='name'>Sova</h3>
-            <p className='description'>My name is Sova. I'm the hunter. I like to use old weapens.</p>
+            <p className='description'>I'm the hunter. I like to use old weapens.</p>
         </div>
     )
 }
