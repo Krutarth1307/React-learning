52deb584d746e3395a9a0b4a0ac64f6a72afdb5e
Krutarth <krutarth1307@gmail.com>
2026-06-08 23:47:17 +0530

    Conditions in event click litsner.

--- my-react-app/src/ClickEvent.jsx
+++ my-react-app/src/ClickEvent.jsx
@@ -2,9 +2,18 @@
 
 function ClickEvent (){
 
-    const click = () => console.log(`Succesfully clicked.`)
+    let count = 0;
+    const click = (name) =>{
+        if(count < 3){
+            count ++;
+            console.log(`Succesfully clicked ${count} times.`)
+        }
+        else{
+            console.log(`Stop clicking me ${name}`)
+        }}
+
     return(
-        <button onClick= {click}>Click me...</button>
+        <button onClick ={() => click('Krutarth')}>Click me...</button>
     )
 }
 
