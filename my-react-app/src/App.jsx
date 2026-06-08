112d8a1ddf240df48c6124d9d176eb7303c06198
Krutarth <krutarth1307@gmail.com>
2026-06-08 23:40:31 +0530

    Started learning event litstner.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,24 +2,39 @@ import List from "./assets/List";
 import Props from "./Props";
 import sova from "./assets/Sova.png";
 import jett from "./assets/Jett.png";
+import ClickEvent from "./ClickEvent";
 
 
 function App(){
 
-
        return(
               <>
-                     <Props icon= {sova}name="Sova" description = "I'm the hunter."/>
-
-                     <Props icon= {sova}name="Sova" description = "I use old weapons."/>
-
-                     <Props icon= {jett}name="Jett" description = "I love wind."/>
-
-                     <Props icon= {sova}name="Sova" description = "I'm the hunter."/>
-
-                     <Props />
+                     <ClickEvent />
               </>
        )
+
+
+
+
+
+       // return(
+       //        <>
+       //               <Props icon= {sova}name="Sova" description = "I'm the hunter."/>
+
+       //               <Props icon= {sova}name="Sova" description = "I use old weapons."/>
+
+       //               <Props icon= {jett}name="Jett" description = "I love wind."/>
+
+       //               <Props icon= {sova}name="Sova" description = "I'm the hunter."/>
+
+       //               <Props />
+       //        </>
+       // )
+
+
+
+
+
        // const fruits = [{id : 1, name : "apple" , calories : 50},
        //               {id : 2, name : "orange" ,calories : 62},
        //               {id : 3, name : "banana" ,calories : 45},
---
+++ my-react-app/src/ClickEvent.jsx
@@ -0,0 +1,11 @@
+
+
+function ClickEvent (){
+
+    const click = () => console.log(`Succesfully clicked.`)
+    return(
+        <button onClick= {click}>Click me...</button>
+    )
+}
+
+export default ClickEvent 
