c69c58a0d9f2eeac87cc85054458e8f9c537e440
Krutarth <krutarth1307@gmail.com>
2026-06-08 23:52:18 +0530

    Learned event litsner with content change.

--- my-react-app/src/ClickEvent.jsx
+++ my-react-app/src/ClickEvent.jsx
@@ -3,17 +3,10 @@
 function ClickEvent (){
 
     let count = 0;
-    const click = (name) =>{
-        if(count < 3){
-            count ++;
-            console.log(`Succesfully clicked ${count} times.`)
-        }
-        else{
-            console.log(`Stop clicking me ${name}`)
-        }}
+    const click = (e) => e.target.textContent = `Ouch!`;
 
     return(
-        <button onClick ={() => click('Krutarth')}>Click me...</button>
+        <button onClick ={(e) => click(e)}>Click me...</button>
     )
 }
 
